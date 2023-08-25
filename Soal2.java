82d15ac0d36524281e262021bd4937a48304ce37
Mutia Hardita <118504168+tiaditaa@users.noreply.github.com>
2023-08-25 19:06:56 +0700

    Update Soal2.java

--- Soal2.java
+++ Soal2.java
@@ -4,7 +4,7 @@ public class Soal2 {
 	
 	public static void main(String[] args) {
 		int i = 42;
-		String s = (i<40)?"life":(i>50)?"universe":"everything";
+		String s = (i<40)?"life":(i>50)?"universe":"everything"; //output yang dihasilkan sesuai dengan i yang diketahui, teknik yang digunakan bernama operator ternary
 		System.out.println(s);
 	}
 }
